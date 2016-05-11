f60735adbc16bd2b7bb6e5229b373e95d2deefda
kolyjjj <me.koly.li@gmail.com>
2016-05-11 21:41:48 +0800

    use superagent to fire http service request

--- src/login.js
+++ src/login.js
@@ -1,9 +1,17 @@
 'use strict';
 
+import request from 'superagent';
+
+const useServiceIp = 'http://localhost:10301';
+
 const login = (req, res, next) => {
   //TODO if authenticated, then do nothing, maybe return some code
-    console.log('I am login module');
-    res.send('loging....');
+  request.post(useServiceIp + '/users')
+    .end((err, response) => {
+      console.log('response', response.body, err);
+      if (err) res.status(500).json(err);
+      res.status(200).json(response.body);
+    });
 };
 
 export default login;

317e6f9275c5e88399201612583173812848cba8
kolyjjj <me.koly.li@gmail.com>
2016-05-11 18:55:15 +0800

    add gulp sourcemaps and concat

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,10 +2,15 @@
 
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import sourcemaps from 'gulp-sourcemaps';
+import concat from 'gulp-concat';
 
 gulp.task('compile', _ => {
     return gulp.src(['index.js', 'src/**/*.js'])
+      .pipe(sourcemaps.init())
       .pipe(babel())
+      .pipe(concat('all.js'))
+      .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest('dist'));
 });
 

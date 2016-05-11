2a10e706806e4b33563188f82c5db9dd1b787310
kolyjjj <me.koly.li@gmail.com>
2016-05-11 19:00:40 +0800

    use nodemon to run the app

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import gulp from 'gulp';
 import babel from 'gulp-babel';
 import sourcemaps from 'gulp-sourcemaps';
 import concat from 'gulp-concat';
+import nodemon from 'gulp-nodemon';
 
 gulp.task('compile', _ => {
     return gulp.src(['index.js', 'src/**/*.js'])
@@ -12,6 +13,14 @@ gulp.task('compile', _ => {
       .pipe(concat('all.js'))
       .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest('dist'));
+});
+
+gulp.task('start', _ => {
+  return nodemon({
+      script: 'dist/all.js',
+      watch: ['index.js', 'src/**/*.js'],
+      tasks: ['compile']
+  });  
 });
 
 gulp.task('default', _ => {

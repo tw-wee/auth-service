b463005227027a845248e9d3f06ab81be0e1fbec
kolyjjj <me.koly.li@gmail.com>
2016-05-11 18:47:07 +0800

    use babel to compile src

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,13 @@
 'use strict';
 
 import gulp from 'gulp';
+import babel from 'gulp-babel';
+
+gulp.task('compile', _ => {
+    return gulp.src(['index.js', 'src/**/*.js'])
+      .pipe(babel())
+      .pipe(gulp.dest('dist'));
+});
 
 gulp.task('default', _ => {
   console.log('default task');

525a873d7a4ec5b7f252f1f98ffcc0b78fef404c
kolyjjj <me.koly.li@gmail.com>
2016-05-23 22:21:01 +0800

    use lodash as utils

--- src/routers.js
+++ src/routers.js
@@ -3,12 +3,20 @@
 import express from 'express';
 import login from './login';
 import logout from './logout';
+import utils from './utils';
 
 const routers = express.Router();
+
+routers.get('/login', (req, res) => {
+    res.status(200).send('Hello, this is login page');
+});
 
 routers.post('/login', login);
 routers.post('/logout', logout);
 
 // methods below should be accessed with api-key
+routers.use((req, res, next) => {
+    if (utils.isEmpty(req.get('wee-key'))) return res.status(401).json();
+});
 
 export default routers;
---
+++ src/utils.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import _ from 'lodash';
+
+const utils = {
+   isEmpty: val => {
+      return _.isEmpty;
+   }
+};
+
+export default utils;

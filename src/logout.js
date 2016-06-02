4bdd5c7ccd09fdafac8083af3f445b66377ace0b
kolyjjj <me.koly.li@gmail.com>
2016-06-02 20:14:14 +0800

    add log out

--- src/logout.js
+++ src/logout.js
@@ -1,11 +1,15 @@
 'use strict';
 
 import redis from './redis';
+import apikey from './apikey';
 
 const logout = (req, res, next) => {
-    //TODO: if not login, return 401
-    console.log('logging out');
-    res.send('logging out');
+  if (!apikey.existsIn(req)) return res.status(401).json();
+
+  const key = apikey.getKeyFrom(req);
+  console.log('logging out', key);
+  redis.expire(key, 1);
+  res.status(200).json();
 };
 
 export default logout;

6d5f3f61040980bb0aa02d69baa7b6be53bc086e
kolyjjj <me.koly.li@gmail.com>
2016-06-09 21:54:54 +0800

    return 400 when there is api key in login request

--- src/login.js
+++ src/login.js
@@ -1,15 +1,16 @@
 'use strict';
 
 import request from 'superagent';
-import redis from './redis';
 import uuid from 'node-uuid';
 import cache from './cache';
+import apikey from './apikey';
 
 const useServiceIp = 'http://localhost:10301';
 const expireTime = 24*60*60; // 24 hours 
 
 const login = (req, res, next) => {
   //TODO if authenticated, then do nothing, maybe return some code
+  if (apikey.existsIn(req)) return res.status(400).json();
   request.post(useServiceIp + '/users')
     .end((err, response) => {
       console.log('response', response.body, err);
